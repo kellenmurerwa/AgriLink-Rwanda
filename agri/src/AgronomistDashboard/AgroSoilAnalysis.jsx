2a03197e788ae571ac03d45a8678f7d22eedb172
Kellen Murerwa <145454946+kellenmurerwa@users.noreply.github.com>
2025-03-24 12:31:36 +0200

    Agro set up

---
+++ agri/src/AgronomistDashboard/AgroSoilAnalysis.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "../AgronomistStyles/AgroSoilAnalysis.css"; 
+
+const AgroSoilAnalysis = () => {
+  
+};
+
+export default AgroSoilAnalysis;
